ac29b11684763d6798bf5bb666178df317e9f089
한진호 <tlfanfvk@naver.com>
2020-09-20 22:46:46 +0900

    Add PostsDto & PostsService

--- src/main/java/com/brick/boot/spring/web/PostsApiController.java
+++ src/main/java/com/brick/boot/spring/web/PostsApiController.java
@@ -1,20 +1,32 @@
 package com.brick.boot.spring.web;
 
-import com.brick.boot.spring.service.posts.PostService;
+import com.brick.boot.spring.service.posts.PostsService;
+import com.brick.boot.spring.web.dto.PostsResponseDto;
 import com.brick.boot.spring.web.dto.PostsSaveRequestDto;
+import com.brick.boot.spring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class PostsApiController {
 
-    private final PostService postService;
+    private final PostsService postsService;
 
-    @GetMapping("/api/v1/posts")
+
+    @PostMapping("/api/v1/posts/")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
-        return postService.save(requestDto);
+        return postsService.save(requestDto);
+    }
+
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
